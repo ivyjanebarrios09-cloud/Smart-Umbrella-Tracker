373d69373602d3dc2ba1abb1919525999505b6a9
Ivy Jane <ivyjane033@gmail.com>
2025-11-19 12:26:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8038064895-ef9a4",
+  "appId": "1:508356808964:web:1c51d45c4646ec16748051",
+  "apiKey": "AIzaSyAdSewwWIfuWtyOznCtZfb3tF94iSw0uz0",
+  "authDomain": "studio-8038064895-ef9a4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "508356808964"
+};
